b7c01b0cd49b06b53143e2842bd544e36c6dc2ce
Michael Shepherd M <shepherd.munemo@outlook.com>
2024-07-11 16:52:34 +0200

    Binary search Tree

--- BinaryTree/binarySearchTree.py
+++ BinaryTree/binarySearchTree.py
@@ -90,7 +90,7 @@ class BinarySearchTree:
         # Return a string of all the Nodes using in order traversal
         return str(self.root)
         
-    def __reassgn_nodes(self, node: Node, new_children: Node | Node) -> None:
+    def __reassign_nodes(self, node: Node, new_children: Node | Node) -> None:
         if new_children is not None: # reset its kids
             new_children.parent = node.parent
         if node.parent is not None: #reset its parent
@@ -104,5 +104,129 @@ class BinarySearchTree:
     def empty(self) -> bool:
         return self.root is None
     
+    def __insert(self, value) -> None:
+        """
+        Insert a new node in Binary Search Treewith value label
+        """
+        new_node = Node(value) # create a new Node
+        if self.empty(): # if tree is empty
+            self.root = new_node #set its root
+        else: # tree is not empty
+            parent_node = self.root # from root
+            if parent_node is None:
+                return
+            while True: # While we don't get to a leaf
+                if value < parent_node.value: # We go left
+                    if parent_node.left is None:
+                        parent_node.left = new_node # we insert the new node in a leaf
+                        break
+                    else:
+                        parent_node = parent_node.left
+                else:
+                    if parent_node.right is None:
+                        parent_node.right = new_node
+                        break
+                    else:
+                        parent_node = parent_node.right
+            new_node.parent = parent_node
+        
+    def insert(self, *values) -> None:
+        for value in values:
+            self.__insert(value)
+            
+    def search(self, value) -> Node | None:
+        if self.empty():
+            raise IndexError("Warning: Tree is empty! please us another.")
+        else:
+            node = self.root
+            #use lazy evaluation here to avide NodeType Attribute error
+            while node is not None and node.value is not value:
+                node = node.left if value < node.value else node.right
+            return node
+        
+    def get_max(self, node: Node | Node = None) -> Node | None:
+        """
+        We go deeper into the right branch
+        """
+        if node is None:
+            if self.root is None:
+                return None
+            node = self.root
+            
+        if not self.empty():
+            while node.right is not None:
+                node = node.right
+        return node
     
+    def get_min(self, node: Node | None = None) -> Node | None:
+        # Deeper to the left branch
+        if node is None:
+            node = self.root
+        if self.root is None:
+            return None
+        if not self.empty():
+            node = self.root
+            while node.left is not None:
+                node = node.left
+        return node
+    
+    def remove(self, value: int) -> None:
+        # Look for the node with that label
+        node = self.search(value)
+        if node is None:
+            msg = f"Value {value} not found"
+            raise ValueError(msg)
+        
+        if node.left is None and node.right is None: # it has no children
+            self.__reassign_nodes(node, None)
+        elif node.left is None: # Has only right child
+            self.__reassign_nodes(node, node.right)
+        elif node.right is None: # Has only left child
+            self.__reassign_nodes(node, node.left)
+        else:
+            predecessor = self.get_max(
+                node.left
+            ) # Gets the max value of the left branch
+            self.remove(predecessor.value) # type: ignore
+            node.value = (
+                predecessor.value # type: ignore
+            ) # Assigns the value to the node to delete and keep tree structure
+            
+    def preorder_traverse(self, node: Node | None) -> Iterable:
+        if node is not None:
+            yield node # Preorder Traversal
+            yield from self.preorder_traverse(node.left)
+            yield from self.preorder_traverse(node.right)
+            
+    def traversal_tree(self, traversal_function = None) -> Any:
+        # this func traversal the tree. You can pass a func to traversal the tree as needed by client code
+        if traversal_function is None:
+            return self.preorder_traverse(self.root)
+        else:
+            return traversal_function(self.root)
+        
+    def inorder(self, arr: list, node: Node | None) -> None:
+        # Perfom an inorder traversal and append values of the nodes to a list named arr
+        if node:
+            self.inorder(arr, node.left)
+            arr.append(node.value)
+            self.inorder(arr, node.right)
+            
+    def find_kth_smallest(self, k: int, node: Node) -> int:
+        # return the kth smallest element in a binary search tree
+        arr: list[int] = []
+        self.inorder(arr, node) # append all values to list using inorder traversal
+        return arr[k - 1]
+            
+    def postorder(curr_node: Node | Node) -> list[Node]:
+        # postOrder(left, right, self)
+        node_list = []
+        if curr_node is not None:
+            node_list = postorder(curr_node.left) + postorder(curr_node.right) + [curr_node]
+        return node_list
+    
+    if __name__ == "__main__":
+        import doctest
+        
+        doctest.testmod(verbose=True)
             
